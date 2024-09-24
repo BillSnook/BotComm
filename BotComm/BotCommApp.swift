488a4104f3135ff156fe08e86605d5bf14e1b475
Bill <billsnook@me.com>
2024-09-24 14:26:53 -0700

    Initial Commit

---
+++ BotComm/BotCommApp.swift
@@ -0,0 +1,17 @@
+//
+//  BotCommApp.swift
+//  BotComm
+//
+//  Created by Bill Snook on 9/24/24.
+//
+
+import SwiftUI
+
+@main
+struct BotCommApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BotCommTests/BotCommTests.swift
@@ -0,0 +1,17 @@
+//
+//  BotCommTests.swift
+//  BotCommTests
+//
+//  Created by Bill Snook on 9/24/24.
+//
+
+import Testing
+@testable import BotComm
+
+struct BotCommTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
