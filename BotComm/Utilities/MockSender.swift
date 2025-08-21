9a88bd345d7203b0de6542f1923cfaf3783a0161
Bill <billsnook@me.com>
2025-08-21 16:48:24 -0700

    Long time, many  experiemntal changes

---
+++ BotComm/Utilities/MockSender.swift
@@ -0,0 +1,1 @@
+ = runMode.buttonTest() ? .connected : .disconnected
--- BotComm/ViewControllers/StartupView.swift
+++ BotComm/ViewControllers/StartupView.swift
@@ -1,5 +1,5 @@
 //
-//  ContentView.swift
+//  StartupView.swift
 //  BotComm
 //
 //  Created by Bill Snook on 9/24/24.
@@ -7,7 +7,7 @@
 
 import SwiftUI
 
-struct ContentView: View {
+struct StartupView: View {
     var body: some View {
         VStack {
             Image(systemName: "globe")
@@ -20,5 +20,5 @@ struct ContentView: View {
 }
 
 #Preview {
-    ContentView()
+    StartupView()
 }
